9bc517bb204e5edc212806c94c4f86069a12b301
NeekPizza <neekpizza@gmail.com>
2017-07-06 16:48:53 -0700

    initial commit

---
+++ includes/layouts/header.php
@@ -0,0 +1,16 @@
+<?php 
+  if(!isset($layout_context)) {
+    $layout_context = "public";
+  }
+?>
+<head>
+    <title>Widget Corp <?php if($layout_context == "admin"){echo "Admin";}  ?></title>
+    <link href="css/public.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+
+    <div id="header">
+      <h1>Widget Corp <?php if($layout_context == "admin"){echo "Admin";}  ?></h1>
+      
+
+    </div>
